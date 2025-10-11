2cedc82c36ecca029ed0c8321ab890d1f61e156b
AnatolyGeorgievski <AnatolyGeorgievski@gmail.com>
2025-10-11 12:24:02 +0300

    OpenCL-Benchmark Compatible with original/ Добавлены тесты DP4A, DPAS and memory test for Warp-Group Read/Write

---
+++ OpenCL-Benchmark/kernel.cpp
@@ -0,0 +1,296 @@
+#include "kernel.hpp" // note: unbalanced round brackets () are not allowed and string literals can't be arbitrarily long, so periodically interrupt with )+R(
+string opencl_c_container() { return R( // ########################## begin of OpenCL C code ####################################################################
+
+
+
+int dp4a(const char4 a, const char4 b, const int c) { // 4-wide byte dot product and accumulate
+)+"#if cl_nv_compute_capability>=61"+R( // use hardware-supported dp4a on Nvidia Pascal or newer GPUs with inline PTX assembly
+	int d;)+"asm(\"dp4a.s32.s32\t%0,%1,%2,%3;\":\"=r\"(d):\"r\"(as_int(a)),\"r\"(as_int(b)),\"r\"(c));"+R(return d;
+)+"#elif defined(__opencl_c_integer_dot_product_input_4x8bit)"+R( // use hardware-supported dp4a on some Intel GPUs
+	return  c+dot(a,b); // c+dot(a, b);   dot_acc_sat(a, b, c); -- is slow
+)+"#elif __has_builtin(__builtin_amdgcn_sdot4)"+R( // use hardware-supported dp4a on some AMD GPUs
+    return __builtin_amdgcn_sdot4(as_int(a), as_int(b), c, false);
+)+"#elif defined(cl_arm_integer_dot_product_accumulate_int8)"+R( // use hardware-supported dp4a on some ARM GPUs
+    return arm_dot_acc(a, b, c);
+)+"#else"+R( // fallback emulation (compilers will turn this into hardware-supported dp4a instruction if available)
+	return c + a.x*b.x+a.y*b.y+a.z*b.z+a.w*b.w;
+)+"#endif"+R(
+}
+
+
+
+)+"#ifdef cl_khr_fp64"+R( // OpenCL C defines don't work in R() stringification macro
+kernel void kernel_double(global float* data) {
+	double x = (double)get_global_id(0);
+	double y = (double)get_local_id(0);
+	for(uint i=0u; i<128u; i++) {
+		x = fma(y, x, y); // 2 operations
+		y = fma(x, y, x); // 2 operations
+	}
+	data[get_global_id(0)] = (float)y;
+}
+)+"#endif"+R( // cl_khr_fp64
+
+kernel void kernel_float(global float* data) {
+	float x = (float)get_global_id(0);
+	float y = (float)get_local_id(0);
+	for(uint i=0u; i<512u; i++) {
+		x = fma(y, x, y); // 2 operations
+		y = fma(x, y, x); // 2 operations
+	}
+	data[get_global_id(0)] = y;
+}
+
+)+"#ifdef cl_khr_fp16"+R( // OpenCL C defines don't work in R() stringification macro
+kernel void kernel_half(global float* data) {
+	half2 x = (half2)((float)get_global_id(0), (float)get_local_id(0));
+	half2 y = (half2)((float)get_local_id(0), (float)get_global_id(0));
+	float z = 0;//
+	for(uint i=0u; i<512u; i++) {
+		x = y*x+y; // 4 operations
+		y = x*y+x; // 4 operations
+	}
+	data[get_global_id(0)] = (float)y.x+(float)y.y;
+}
+)+"#endif"+R( // cl_khr_fp16
+
+kernel void kernel_long(global float* data) {
+	long x = (long)get_global_id(0);
+	long y = (long)get_local_id(0);
+	for(uint i=0u; i<8u; i++) {
+		x = y*x+y; // 2 operations
+		y = x*y+x; // 2 operations
+	}
+	data[get_global_id(0)] = as_float((int)y);
+}
+
+kernel void kernel_int(global float* data) {
+	int x = get_global_id(0);
+	int y = get_local_id(0);
+	for(uint i=0u; i<512u; i++) {
+		x = y*x+y; // 2 operations
+		y = x*y+x; // 2 operations
+	}
+	data[get_global_id(0)] = as_float(y);
+}
+kernel void kernel_mod_uint(global float* data) {
+	int ix = get_sub_group_local_id();
+	int x = as_int(data[ix+get_sub_group_id()*get_max_sub_group_size()+128]);
+	const ushort A = 0xFFA0;
+	const int P = (A<<16)+1;
+	for(uint i=0u; i<256u; i++) {
+		short2 d = as_short2(data[ix]);
+		x+= d.s0;
+		x = as_short2(x).s1 - as_ushort2(x).s0*A; // 2 operations
+		x+= d.s1;
+		x = as_short2(x).s1 - as_ushort2(x).s0*A; // 2 operations
+//		x = as_ushort2(x).s0*A + as_ushort2(x).s1; // 2 operations
+//		x = as_ushort2(x).s0*A + as_ushort2(x).s1; // 2 operations
+//		x = min(as_uint(x-P), x);
+	}
+	int s1 = sub_group_reduce_add(x>>16);
+	uint s0 = sub_group_reduce_add(x & 0xFFFFu);
+//	x = min(as_uint(x-P), x);
+	if (ix==0) {
+		s1 = s1+(s0>>16) - (s0&0xFFFFu)*A;
+		data[get_sub_group_id()] = as_float(s1);
+	}
+}
+kernel void kernel_shoup_uint(global float* data) {
+	int ix = get_sub_group_local_id();
+	uint x = as_uint(data[ix+get_sub_group_id()*get_max_sub_group_size()+128]);
+	const ushort A = 0xFFA0;
+	const uint P = (A<<16)+1;
+	ulong r = 0;
+	uint2 d = (uint)(data[ix]);
+	for(uint i=0u; i<256u; i++) {
+		uint q = mul_hi(x, d.s1);
+		uint r0= x*d.s0;
+		uint r1= q*P;
+		r0 -=r1;
+		r+= min(r0, r0-P);
+	}
+	uint s1 = sub_group_reduce_add(x>>16);
+	uint s0 = sub_group_reduce_add(x & 0xFFFFu);
+	if (ix==0) {
+		s1 = s1+(s0>>16) - (s0&0xFFFFu)*A;
+		data[get_sub_group_id()] = as_float(s1);
+	}
+}
+kernel void kernel_tnn_uint(global float* data) {
+	int ix = get_sub_group_local_id();
+	ushort2 x = as_ushort2(data[ix+get_sub_group_id()*get_max_sub_group_size()]);
+	ushort2 d = 0;
+	float v = data[ix];
+	for(uint i=0u; i<512u; i++) {
+		ushort2 y = as_ushort2(v);
+		ushort2 r0 = x&y;
+		ushort2 r1 = x&y.s10;
+		x = r1;
+		d += popcount(r0);
+		d -= popcount(r1);
+	}
+	data[get_global_id(0)] = d.x+d.y;
+//	uint s = sub_group_reduce_add(d.x+d.y);
+//	if (ix==0) 
+//		data[get_sub_group_id()] = as_float(s);
+}
+kernel void kernel_short(global float* data) {
+	short2 x = as_short2((uint)get_global_id(0));
+	short2 y = as_short2((uint)get_local_id(0));
+	for(uint i=0u; i<512u; i++) {
+		x = y*x+y; // 4 operations
+		y = x*y+x; // 4 operations
+	}
+	data[get_global_id(0)] = as_float(y);
+}
+kernel void kernel_mod_short(global float* data) {
+	short2 x = as_short2((uint)get_global_id(0));
+	short2 y = as_short2((uint)get_local_id(0));
+	for(uint i=0u; i<128u; i++) {
+		x = (y%x)+y; // 4 operations
+		y = (x%y)+x; // 4 operations
+	}
+	data[get_global_id(0)] = as_float(y);
+}
+
+kernel void kernel_char(global float* data) {
+	char4 x = as_char4((uint)get_global_id(0));
+	char4 y = as_char4((uint)get_local_id(0));
+	for(uint i=0u; i<512u; i++) {
+		x = as_char4(dp4a(y, x, as_int(y))); // 8 operations
+		y = as_char4(dp4a(x, y, as_int(x))); // 8 operations
+	}
+	data[get_global_id(0)] = as_float(y);
+}
+//cl_khr_subgroup_clustered_reduce
+//	int a = dot((char4){ 0, 2,0, 0}, d);
+//	float c = sub_group_clustered_reduce_add(a*u,4)*0.5f;
+// Кубическая интерполяция по четырем точкам
+static inline float cubic_u8_interp(uchar4 d, float dx){
+	float4 u = {1.f,dx, dx*dx, dx*dx*dx};
+	int4 a; 
+	a[0] = dot((char4){ 0, 2,0, 0},d);
+	a[1] = dot((char4){-1, 0,1, 0},d);
+	a[2] = dot((char4){ 2,-5,4,-1},d);
+	a[3] = dot((char4){-1, 3,-3,1},d);
+	return (a[0]*u[0] + a[1]*u[1] + a[2]*u[2] + a[3]*u[3])*0.5f;
+}
+static inline uint Sum0(uint x) {
+	return rotate(x,32u-2) ^ rotate(x,32u-13) ^ rotate(x,32u-22);
+}
+static inline uint Sum1(uint x) {
+	return rotate(x,32u-6) ^ rotate(x,32u-11) ^ rotate(x,32u-25);
+}
+static inline uint Sbox(uint x) {
+	return (x ^ (rotate(~x,1u) & rotate(x,2u)));
+}
+kernel void kernel_sigma(global float* data) {
+	uint x = get_global_id(0);
+	uint y = get_local_id(0);
+	for(uint i=0u; i<512u; i++) {
+#if 1
+		x = Sum0(x)+y;
+		y = Sum1(y)+x;
+#else
+		x = Sbox(x)+y;
+		y = Sbox(y)+x;
+#endif
+	}
+	data[get_global_id(0)] = as_float(y);
+}
+static inline float _matrix_mad_f16_f16_k32( int v, int8 b, float acc )
+{
+)+"#if defined(cl_intel_subgroup_matrix_multiply_accumulate)"+R(
+#if 0//
+	acc = intel_sub_group_f16_f16_matrix_mad_k16(as_short2(v).x,b,acc);
+	acc = intel_sub_group_f16_f16_matrix_mad_k16(as_short2(v).y,b,acc);
+	return acc;
+#elif 0
+	acc = intel_sub_group_bf16_bf16_matrix_mad_k16(as_short2(v).x,b,acc);
+	acc = intel_sub_group_bf16_bf16_matrix_mad_k16(as_short2(v).y,b,acc);
+	return acc;
+#elif 0
+//	acc = as_int(intel_sub_group_u2_u4_matrix_mad_k64(as_uchar4(v).x,as_uint8(b),as_int(acc)));
+//	acc = as_int(intel_sub_group_u2_i8_matrix_mad_k32(as_uchar4(v).x,as_int8(b),as_int(acc)));
+	acc = as_int(intel_sub_group_u8_u8_matrix_mad_k32(as_ushort2(v).x,as_uint8(b),as_int(acc)));
+	acc = as_int(intel_sub_group_u8_u8_matrix_mad_k32(as_ushort2(v).y,as_uint8(b),as_int(acc)));
+	return acc;
+#elif 1
+	return intel_sub_group_tf32_tf32_matrix_mad_k8(as_float(v),as_float8(b),acc);
+#elif 1
+	acc = as_int(intel_sub_group_u8_u8_matrix_mad_k32(as_ushort2(v).x,as_uint8(b),as_int(acc)));
+	acc = as_int(intel_sub_group_u8_u8_matrix_mad_k32(as_ushort2(v).y,as_uint8(b),as_int(acc)));
+	return acc;
+#endif
+)+"#else"+R(
+    float result = acc;
+    for (int k=0;k<8; ++k)
+        result += dot(as_half2(sub_group_broadcast( v, k )), as_half2( b[k] ));
+    return result;
+)+"#endif"+R(
+}
+static inline float _matrix_mad_bf16_bf16_k16( int v, int8 b, float acc )
+{
+}
+__attribute__((intel_reqd_sub_group_size(32)))
+kernel void kernel_mma(global float8* data) {
+	int8 x = get_global_id(0);
+	int8 y = get_local_id(0);
+	for(uint i=0u; i<8u; i++) { y[i]+=i; x[i] *=i; }
+	for(uint r=0u; r<64u; r++)
+	for(uint i=0u; i<8u; i++) {// 512 rounds x 2*k16
+		x[i] = as_int(_matrix_mad_f16_f16_k32(x[i],y,as_float(x[i])));
+		y[i] = as_int(_matrix_mad_f16_f16_k32(y[i],x,as_float(y[i])));
+	}
+	data[get_global_id(0)] = as_float(y[0])+as_float(y[1])+as_float(y[2])+as_float(y[3])+as_float(y[4])+as_float(y[5])+as_float(y[6])+as_float(y[7]);
+}
+inline float _sub_group_block_read(global float* yy){
+    float y;
+    int ix = get_sub_group_local_id();
+    return yy[ix];
+}
+inline float _sub_group_block_write(global float* yy, float val){
+    int ix = get_sub_group_local_id();
+    return yy[ix] = val;
+}
+kernel void kernel_block_read(global float* data) {
+	const uint n = sub_group_broadcast(get_global_id(0),0);
+	const uint sz = get_max_sub_group_size();
+	float x = 0.0f;
+	for(uint i=0u; i<def_M; i++) x += _sub_group_block_read(data+i*sz+n);
+	x = sub_group_reduce_add(x);
+	if (get_sub_group_local_id()==0) 
+		data[n] = x;
+}
+kernel void kernel_block_write(global float* data) {
+	const uint n = sub_group_broadcast(get_global_id(0),0);
+	const uint sz = get_max_sub_group_size();
+	int ix = get_sub_group_local_id();
+	for(uint i=0u; i<def_M; i++) _sub_group_block_write(data+i*sz+n, as_float(ix)); // block write
+}
+
+kernel void kernel_coalesced_write(global float* data) {
+	const uint n = get_global_id(0);
+	for(uint i=0u; i<def_M; i++) data[i*def_N+n] = as_float(n); // coalesced write
+}
+kernel void kernel_coalesced_read(global float* data) {
+	const uint n = get_global_id(0);
+	float x = 0.0f;
+	for(uint i=0u; i<def_M; i++) x += data[i*def_N+n]; // coalesced read
+	data[n] = x;
+}
+kernel void kernel_misaligned_write(global float* data) {
+	const uint n = get_global_id(0);
+	for(uint i=0u; i<def_M; i++) data[n*def_M+i] = as_float(n); // misaligned write
+}
+kernel void kernel_misaligned_read(global float* data) {
+	const uint n = get_global_id(0);
+	float x = 0.0f;
+	for(uint i=0u; i<def_M; i++) x += data[n*def_M+i]; // misaligned read
+	data[n] = x;
+}
+
+
+
+);} // ############################################################### end of OpenCL C code #####################################################################
---
+++ OpenCL-Benchmark/main.cpp
@@ -0,0 +1,264 @@
+#include "opencl.hpp" // includes "utilities.hpp"
+
+string fraction(const float x) {
+	float values[]   = { 1.0f/64.0f, 1.0f/32.0f, 1.0f/24.0f, 1.0f/16.0f, 1.0f/12.0f, 1.0f/8.0f, 1.0f/4.0f, 1.0f/3.0f, 1.0f/2.0f, 2.0f/3.0f, 
+		1.0f, 2.0f, 3.0f, 4.0f, 8.0f, 12.0f, 16.0f, 32.0f, 64.0f };
+	string strings[] = {   "1/64"  ,   "1/32"  ,   "1/24"  ,   "1/16"  ,   "1/12"  ,   "1/8 ",   "1/4 ", "1/3 ", "1/2 ", "2/3 ", 
+		" 1x ", " 2x ", " 3x ", " 4x ", " 8x ", " 12x", " 16x", " 32x", " 64x" };
+	uint i=0u, imin=0u;
+	float vmin = max_float;
+	for(float v : values) {
+		const float vnew = sq(0.01f*x-v);
+		if(vnew<=vmin) {
+			vmin = vnew;
+			imin = i;
+		}
+		i++;
+	}
+	return "("+strings[imin]+")";
+}
+
+void benchmark_device(const Device_Info& device_info) {
+	const uint N = 4096u*4096u; // kernel range: N*M*sizeof(float) = 1GB memory allocation
+	const uint M = 16u; // coalescence size
+	const uint N_kernel = 256u; // iterations for kernel calls
+	const uint N_memory = 16u; // iterations for PCIe memory transfers
+
+	double time_double=max_double, time_float=max_double, time_half=max_double;
+	double time_long=max_double, time_int=max_double, time_short=max_double, time_char=max_double, time_sum=max_double, time_mod=max_double;
+	double time_cw=max_double, time_cr=max_double, time_mw=max_double, time_mr=max_double, time_send=max_double, time_receive=max_double, time_bidirectional=max_double;
+	Clock clock;
+
+	const string defines =
+		"\n	#define def_N "+to_string(N)+"u"
+		"\n	#define def_M "+to_string(M)+"u"
+	;
+	print("| Compiling ...                                                               |");
+	Device device(device_info, defines+get_opencl_c_code());
+	Memory<float> buffer(device, N, M);
+	//print_info("Device mormory usage: "+to_string(device.info.memory_used)+" MB");
+
+	if(device.info.is_fp64_capable) {
+		print("| Benchmarking ...                                                            |");
+		Kernel kernel_double(device, N, "kernel_double", buffer);
+		for(uint i=0u; i<N_kernel; i++) {
+			clock.start();
+			kernel_double.run();
+			time_double = fmin(clock.stop(), time_double);
+		}
+		const float flops_double = 512.0f*(float)N/(float)time_double*1E-12f;
+		println("\r| FP64  compute "+alignr(45u, to_string(flops_double, 3u))+" TFLOPs/s "+fraction(100.0f*flops_double/device.info.tflops)+" |");
+	} else {
+		println("\r| FP64  compute                                          not supported        |");
+	}
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_float(device, N, "kernel_float", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_float.run();
+		time_float = fmin(clock.stop(), time_float);
+	}
+	const float flops_float = 2048.0f*(float)N/(float)time_float*1E-12f; //const float flops = 32.0f*2.0f*(float)sq(M)*N/(float)timef*1E-12f;
+	println("\r| FP32  compute "+alignr(45u, to_string(flops_float, 3u))+" TFLOPs/s "+fraction(100.0f*flops_float/device.info.tflops)+" |");
+
+	if(device.info.is_fp16_capable) {
+		print("| Benchmarking ...                                                            |");
+		Kernel kernel_half(device, N, "kernel_half", buffer);
+		for(uint i=0u; i<N_kernel; i++) {
+			clock.start();
+			kernel_half.run();
+			time_half = fmin(clock.stop(), time_half);
+		}
+		const float flops_half = 4096.0f*(float)N/(float)time_half*1E-12f;
+		println("\r| FP16  compute "+alignr(45u, to_string(flops_half, 3u))+" TFLOPs/s "+fraction(100.0f*flops_half/device.info.tflops)+" |");
+	} else {
+		println("\r| FP16  compute                                          not supported        |");
+	}
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_long(device, N, "kernel_long", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_long.run();
+		time_long = fmin(clock.stop(), time_long);
+	}
+	const float flops_long = 32.0f*(float)N/(float)time_long*1E-12f;
+	println("\r| INT64 compute "+alignr(45u, to_string(flops_long, 3u))+"  TIOPs/s "+fraction(100.0f*flops_long/device.info.tflops)+" |");
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_int(device, N, "kernel_int", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_int.run();
+		time_int = fmin(clock.stop(), time_int);
+	}
+	const float flops_int = 2048.0f*(float)N/(float)time_int*1E-12f;
+	println("\r| INT32 compute "+alignr(45u, to_string(flops_int, 3u))+"  TIOPs/s "+fraction(100.0f*flops_int/device.info.tflops)+" |");
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_short(device, N, "kernel_short", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_short.run();
+		time_short = fmin(clock.stop(), time_short);
+	}
+	const float flops_short = 4096.0f*(float)N/(float)time_short*1E-12f;
+	println("\r| INT16 compute "+alignr(45u, to_string(flops_short, 3u))+"  TIOPs/s "+fraction(100.0f*flops_short/device.info.tflops)+" |");
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_char(device, N, "kernel_char", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_char.run();
+		time_char = fmin(clock.stop(), time_char);
+	}
+	const float flops_char = 2*4096.0f*(float)N/(float)time_char*1E-12f;
+	println("\r| DP4A  compute "+alignr(45u, to_string(flops_char, 3u))+"  TIOPs/s "+fraction(100.0f*flops_char/device.info.tflops)+" |");
+
+//  George 2024-2025
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_sum(device, N, "kernel_sigma", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_sum.run();
+		time_sum = fmin(clock.stop(), time_sum);
+	}
+	float flops_sum = 1024.0f*(float)N/(float)time_sum*1E-12f;
+	println("\r| SUM   compute "+alignr(45u, to_string(flops_sum, 3u))+"  TBOPs/s "+fraction(100.0f*flops_sum/device.info.tflops)+" |");
+	time_sum=max_double;
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_mod(device, N, "kernel_mod_uint", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_mod.run();
+		time_sum = fmin(clock.stop(), time_sum);
+	}
+	flops_sum = 1024.0f*(float)N/(float)time_sum*1E-12f;
+	println("\r| MOD   compute "+alignr(45u, to_string(flops_sum, 3u))+"  TBOPs/s "+fraction(100.0f*flops_sum/device.info.tflops)+" |");
+	time_sum=max_double;
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_tnn(device, N, "kernel_tnn_uint", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_tnn.run();
+		time_sum = fmin(clock.stop(), time_sum);
+	}
+	flops_sum = 1024.0f*(float)N/(float)time_sum*1E-12f;
+	println("\r| TNN   compute "+alignr(45u, to_string(flops_sum, 3u))+"  TBOPs/s "+fraction(100.0f*flops_sum/device.info.tflops)+" |");
+	time_sum=max_double;
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_mma(device, N, "kernel_mma", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_mma.run();
+		time_sum = fmin(clock.stop(), time_sum);
+	}
+	flops_sum = 16*1024.0f*(float)N/(float)time_sum*1E-12f;
+	println("\r| MMA.h compute "+alignr(45u, to_string(flops_sum, 3u))+"  TIOPs/s "+fraction(100.0f*flops_sum/device.info.tflops)+" |");
+// ~George
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_block_write(device, N, "kernel_block_write"  , buffer);
+	time_cw = max_double;
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_block_write.run();
+		time_cw = fmin(clock.stop(), time_cw);
+	}
+	Kernel kernel_block_read(device, N, "kernel_block_read"  , buffer);
+	time_cr = max_double;
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_block_read.run();
+		time_cr = fmin(clock.stop(), time_cr);
+	}
+	println("\r| Memory Bandwidth ( block read          ) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float)(time_cr-time_cw/(double)M)*1E-9f, 2u))+" GB/s |");
+	println("\r| Memory Bandwidth ( block write         ) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float)(time_cw)*1E-9f, 2u))+" GB/s |");
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_coalesced_write(device, N, "kernel_coalesced_write" , buffer);
+	time_cw = max_double;
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_coalesced_write.run();
+		time_cw = fmin(clock.stop(), time_cw);
+	}
+	Kernel kernel_coalesced_read(device, N, "kernel_coalesced_read"  , buffer);
+	time_cr = max_double;
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_coalesced_read.run();
+		time_cr = fmin(clock.stop(), time_cr);
+	}
+	println("\r| Memory Bandwidth ( coalesced read      ) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float)(time_cr-time_cw/(double)M)*1E-9f, 2u))+" GB/s |");
+	println("\r| Memory Bandwidth ( coalesced      write) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float) time_cw                   *1E-9f, 2u))+" GB/s |");
+
+	print("| Benchmarking ...                                                            |");
+	Kernel kernel_misaligned_write(device, N, "kernel_misaligned_write", buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_misaligned_write.run();
+		time_mw = fmin(clock.stop(), time_mw);
+	}
+	Kernel kernel_misaligned_read(device, N, "kernel_misaligned_read" , buffer);
+	for(uint i=0u; i<N_kernel; i++) {
+		clock.start();
+		kernel_misaligned_read.run();
+		time_mr = fmin(clock.stop(), time_mr);
+	}
+	println("\r| Memory Bandwidth (misaligned read      ) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float)(time_mr-time_cw/(double)M)*1E-9f, 2u))+" GB/s |");
+	println("\r| Memory Bandwidth (misaligned      write) "+alignr(29u, to_string(4.0f*(float)N*(float)M/(float) time_mw                   *1E-9f, 2u))+" GB/s |");
+
+	if(!device.info.uses_ram) {
+		print("| Benchmarking ...                                                            |");
+		for(uint i=0u; i<N_memory; i++) {
+			clock.start();
+			buffer.write_to_device();
+			time_send = fmin(clock.stop(), time_send);
+		}
+		const float bw_send = 4.0f*M*N/(float)time_send*1E-9f;
+		println("\r| PCIe   Bandwidth (send                 ) "+alignr(29u, to_string(bw_send, 2u))+" GB/s |");
+		print("| Benchmarking ...                                                            |");
+		for(uint i=0u; i<N_memory; i++) {
+			clock.start();
+			buffer.read_from_device();
+			time_receive = fmin(clock.stop(), time_receive);
+		}
+		const float bw_receive = 4.0f*M*N/(float)time_receive*1E-9f;
+		println("\r| PCIe   Bandwidth (   receive           ) "+alignr(29u, to_string(bw_receive, 2u))+" GB/s |");
+		print("| Benchmarking ...                                                            |");
+		for(uint i=0u; i<N_memory; i++) {
+			clock.start();
+			buffer.read_from_device(N*M/2u, N*M, false);
+			buffer.write_to_device(0u, N*M/2u, false);
+			buffer.finish_queue();
+			time_bidirectional = fmin(clock.stop(), time_bidirectional);
+		}
+		const float bw_bidirectional = 4.0f*M*N/(float)time_bidirectional*1E-9f;
+		const float bw_max = fmax(2.0f*fmax(bw_send, bw_receive), bw_bidirectional);
+		println("\r| PCIe   Bandwidth (        bidirectional)            (Gen"+to_string(bw_max>17.6f?4:bw_max>8.8f?3:bw_max>4.4f?2:1)+" x16)"+alignr(8u, to_string(bw_bidirectional, 2u))+" GB/s |");
+	}
+
+	println("|-----------------------------------------------------------------------------|");
+}
+
+int main(int argc, char* argv[]) {
+	vector<string> main_arguments = get_main_arguments(argc, argv);
+	println(".-----------------------------------------------------------------------------.");
+	const vector<Device_Info> devices = get_devices();
+	if((int)main_arguments.size()>0) {
+		for(uint i=0u; i<(uint)main_arguments.size(); i++) benchmark_device(select_device_with_id(to_int(main_arguments[i]), devices));
+	} else {
+		for(uint i=0u; i<(uint)devices.size(); i++) benchmark_device(devices[i]);
+	}
+#ifdef _WIN32
+	println("|-----------------------------------------------------------------------------|");
+	println("| Done. Press Enter to exit.                                                  |");
+	println("'-----------------------------------------------------------------------------'");
+	wait();
+#else // Linux
+	println("'-----------------------------------------------------------------------------'");
+#endif // Linux
+	return 0;
+}
